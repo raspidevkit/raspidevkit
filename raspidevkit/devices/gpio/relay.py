93719eb36d4364e7ca7ed70c33ba7a7a6b2c1456
DailyLollipops <clarencemadrigal84@gmail.com>
2024-02-03 17:54:34 +0800

    Add relay

---
+++ raspidevkit/devices/gpio/relay.py
@@ -0,0 +1,77 @@
+import sys
+sys.path.append('../')
+
+from ..base import GpioDevice
+from raspidevkit.constants import OUTPUT
+
+
+class Relay(GpioDevice):
+    def __init__(self, machine, pin: int):
+        """
+        Create a LED object
+
+        :param machine: Machine instance as this device parent
+        :param pin: Pin this device is attached to
+        """
+        pin_setup = {
+            str(pin): OUTPUT
+        }
+        super().__init__(pin_setup, device_type=OUTPUT)
+        self._state = False
+        self._machine = machine
+
+
+
+    @property
+    def state(self):
+        """
+        Device current state
+        """
+        return self._state
+    
+
+    
+    def turn_on(self):
+        """
+        Turn on relay. Has no effect if already turn on.
+        """
+        if not self._state:
+            self._machine.gpio_write(self.pin, True)
+            self._state = True
+
+
+
+    def turn_off(self):
+        """
+        Turn off relay. Has no effect if already turn off.
+        """
+        if self._state:
+            self._machine.gpio_write(self.pin, False)
+            self._state = False
+
+
+
+    def set_state(self, state: bool):
+        """
+        Turn relay on/off.
+
+        :param state: State to switch to
+        """
+        if state:
+            self.turn_on()
+        else:
+            self.turn_off()
+
+
+
+    def cleanup(self):
+        """
+        Perform cleanup
+        """
+        self.turn_off()
+
+
+        
+    def __repr__(self):
+        return f"Relay <pin={self.pin}, state={self._state}>"
+
