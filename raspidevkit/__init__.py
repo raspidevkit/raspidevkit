1162095130427256735640bb604972be7c114504
DailyLollipops <clarencemadrigal84@gmail.com>
2024-02-19 02:16:28 +0800

    Update formatting

--- raspidevkit/__init__.py
+++ raspidevkit/__init__.py
@@ -1,8 +1,9 @@
 from .machine import Machine
-from .constants import BCM, BOARD, RISING, FALLING, BOTH, \
-    INPUT, OUTPUT, PULL_UP, PULL_DOWN, \
-    RED, GREEN, BLUE, YELLOW, MAGENTA, CYAN, WHITE
-
+from .constants import (
+    BCM, BOARD, RISING, FALLING, BOTH, 
+    INPUT, OUTPUT, PULL_UP, PULL_DOWN, 
+    RED, GREEN, BLUE, YELLOW, MAGENTA, CYAN, WHITE,
+)
 
 __all__ = [
     'Machine',
