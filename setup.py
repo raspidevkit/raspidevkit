4aa2b52974b8b0c26c0bf6d736653a9332b2b7f7
DailyLollipops <clarencemadrigal84@gmail.com>
2024-09-12 00:42:29 +0800

    Fix missing package files and libraries

--- setup.py
+++ setup.py
@@ -5,15 +5,21 @@ setuptools.setup(
     description="Easily control devices with Raspberry Pi",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    version="0.0.2",
+    version="0.0.3",
     url="https://github.com/raspidevkit/raspidevkit",
     author="DailyLollipops",
     author_email="clarencemadrigal84@gmail.com",
     packages=setuptools.find_namespace_packages(),
     include_package_data=True,
-    install_requires=['fake-rpi', 
-                      'pyserial', 
-                      'setuptools', 
-                      'smbus2',
-                      'wheel']
+    install_requires=[
+        'fake-rpi', 
+        'pyserial', 
+        'setuptools', 
+        'smbus2',
+        'wheel',
+    ],
+    package_data={
+        '': ['templates/*', 'templates/.clang-format'],
+        'raspidevkit': ['templates/*', 'templates/.clang-format']
+    },
 )
